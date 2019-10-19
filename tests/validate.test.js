acb688b497ea22b0aa136c74ce4b6ab2c3ad067c
Holly Fanthorpe <hollyfanthorpe@gmail.com>
2019-10-19 13:40:11 +0100

    added test & wrote code for password validation - 8 characters

--- tests/validate.test.js
+++ tests/validate.test.js
@@ -28,4 +28,25 @@ describe('validation', () => {
         });
       });
   });
+
+  it('will not create a new user with an invalid password', (done) => {
+    chai.request(server)
+      .post('/filmfinder/users')
+      .send({
+        firstName: 'Holly',
+        lastName: 'Fanthorpe',
+        email: 'holly@test.com',
+        password: 'p',
+      })
+      .end((error, res) => {
+        expect(error).to.equal(null);
+        expect(res.status).to.equal(400);
+        expect(res.body.errors.password).to.equal('Password must be at least 8 characters');
+
+        User.countDocuments({}, (error, count) => {
+          expect(count).to.equal(0);
+          done();
+        });
+      });
+  });
 });
