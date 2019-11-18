0dd7941fb98854a5820edd87ac81c51142f68629
Holly Fanthorpe <hollyfanthorpe@gmail.com>
2019-11-18 18:10:49 +0000

    changed port

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ const app = require('./src/app');
 
 require('dotenv').config();
 
-const port = 8080;
-
 mongoose.connect(process.env.DATABASE_CONN, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }, (err) => {
   console.log(err);
-  app.listen(port, () => {
-    console.log(`App is running on port ${port}`);
+  app.listen(process.env.PORT, () => {
   });
 });
