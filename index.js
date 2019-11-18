49ddb358590632d210064ea21e7cb4c03eba69d4
Holly Fanthorpe <hollyfanthorpe@gmail.com>
2019-11-18 18:25:04 +0000

    added port and host

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const app = require('./src/app');
 
 require('dotenv').config();
 
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+
 mongoose.connect(process.env.DATABASE_CONN, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }, (err) => {
   console.log(err);
-  app.listen(process.env.PORT);
+  app.listen(port, host);
 });
