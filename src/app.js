14561b00213de93c279ad4cb824e7f72d7318f98
Holly Fanthorpe <hollyfanthorpe@gmail.com>
2019-11-18 18:03:41 +0000

    updated app.js

--- src/app.js
+++ src/app.js
@@ -8,6 +8,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello World!' });
+});
+
+
 app.use('/filmfinder', userRouter);
 
 app.use('/filmfinder', movieRouter);
